5ca803b2fbfc6afe26958dc0a924b71c58888f1c
Eimy-Guerra <eimyguerra31@gmail.com>
2025-07-17 16:30:09 -0500

    Actualiza main.py para crear tabla users en PostgreSQL

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from app.database.database import engine
+from app.models import user  # importa el modelo para que SQLAlchemy lo registre
 
 app = FastAPI()
 
+# Crear las tablas en la base de datos
+user.Base.metadata.create_all(bind=engine)
+
 @app.get("/")
 def read_root():
-    return {"mensaje": "¡Hola Eimy, FastAPI está corriendo!"}
+    return {"mensaje": "¡Hola Eimy, FastAPI está corriendo con PostgreSQL!"}
