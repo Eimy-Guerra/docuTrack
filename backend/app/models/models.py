b8c83d192758e128bce341f99b4cac34ab23da2d
Eimy-Guerra <eimyguerra31@gmail.com>
2025-07-18 14:24:56 -0500

    feat(modelos): se define la estructura completa con SQLAlchemy para users, requests, documents, certificates y admins

---
+++ backend/app/models/models.py
@@ -0,0 +1,71 @@
+from sqlalchemy import Column, Integer, String, Date, Enum, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+import enum
+
+class RequestType(str, enum.Enum):
+    nacimiento = "cert_nacimiento"
+    estudios = "cert_estudios"
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String, nullable=False)
+    correo = Column(String, unique=True, index=True)
+    contraseña = Column(String, nullable=False)
+
+    requests = relationship("Request", back_populates="usuario")
+
+
+class Request(Base):
+    __tablename__ = "requests"
+
+    id = Column(Integer, primary_key=True, index=True)
+    tipo = Column(Enum(RequestType), nullable=False)
+    nombre_usuario = Column(String, nullable=False)
+    apellido_usuario = Column(String, nullable=False)
+    fecha_nacimiento = Column(Date, nullable=True)
+    fecha_inicio_estudios = Column(Date, nullable=True)
+    fecha_fin_estudios = Column(Date, nullable=True)
+    estado = Column(String, nullable=False)
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+    usuario = relationship("User", back_populates="requests")
+
+    document = relationship("Document", back_populates="request", uselist=False)
+    certificate = relationship("Certificate", back_populates="request", uselist=False)
+
+
+class Document(Base):
+    __tablename__ = "documents"
+
+    id = Column(Integer, primary_key=True, index=True)
+    dni_path = Column(String, nullable=False)
+
+    request_id = Column(Integer, ForeignKey("requests.id"))
+    request = relationship("Request", back_populates="document")
+
+
+class Certificate(Base):
+    __tablename__ = "certificates"
+
+    id = Column(Integer, primary_key=True, index=True)
+    tipo = Column(Enum(RequestType), nullable=False)
+    titulo = Column(String, nullable=False)
+    nombre_usuario = Column(String, nullable=False)
+    apellido_usuario = Column(String, nullable=False)
+    lugar_estudio = Column(String, nullable=True)
+    fecha_inicio_estudios = Column(Date, nullable=True)
+
+    request_id = Column(Integer, ForeignKey("requests.id"))
+    request = relationship("Request", back_populates="certificate")
+
+
+class Admin(Base):
+    __tablename__ = "admins"
+
+    id = Column(Integer, primary_key=True, index=True)
+    usuario = Column(String, unique=True, index=True)
+    contraseña = Column(String, nullable=False)
+    rol = Column(String, unique=True, nullable=False)  # Verificación única por admin
